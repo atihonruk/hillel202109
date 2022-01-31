049fce8785c6edef712148cb84b7866a4349fb1e
Oleksandr Tykhonruk <a.tihonruk@gmail.com>
2022-01-31 15:13:29 +0200

    Staticfiles support with whitenoise

--- lesson33/docker-entrypoint.sh
+++ lesson33/docker-entrypoint.sh
@@ -2,9 +2,11 @@
 
 sleep 10
 
+echo Collect static files.
+./manage.py collectstatic
 
 echo Run database migrations.
-python ./manage.py migrate
+./manage.py migrate
 
 echo Starting Django.
 gunicorn django_blog.wsgi --bind 0.0.0.0:$PORT
